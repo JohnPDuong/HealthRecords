92f062d43fce2f9994774b674704b6cc12f88e14
jpduong12 <jpduong12@gmail.com>
2022-01-01 11:35:26 -0800

    json object

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,9 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const port = 3000;
+const port = 3001;
+
+app.use(express.json());
 
 var corsOptions = {
     origin: `http://localhost:${port}`
