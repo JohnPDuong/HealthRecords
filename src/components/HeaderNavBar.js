696594b7cbcf370f43f46373a9fbd47825d77e16
jpduong12 <jpduong12@gmail.com>
2022-01-12 23:24:37 -0800

    Expanding on responsive web design

--- src/components/HeaderNavBar.js
+++ src/components/HeaderNavBar.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 import '../css/layout/App.css';
 import '../css/layout/HeaderNavBar.css';
 
-import img from '../img/NewHeaderLogo.png';
+import img from '../img/NewHeaderLogoBlack.png';
 
 export default class HeaderNavBar extends React.Component {
     constructor(props) {
@@ -17,15 +17,46 @@ export default class HeaderNavBar extends React.Component {
 
     render() {
         return (
-            <div className="header-wrapper">
-                <div className="header">
-                    <img src={ img } />
-                </div>
-                <div className="nav-wrapper">
-                    <ul className="navbar">
-                        <li><NavLink exact="true" activestyle={{ color: "red" }} to={ "/" }>Home</NavLink></li>
-                        <li><NavLink exact="true" activestyle={{ color: "red" }} to={ "/news" }>News</NavLink></li>
-                        <li><NavLink exact="true" activestyle={{ color: "red" }} to={ "/about" }>About</NavLink></li>
+            <div className="container-header">
+                <div className="header">he</div>
+                <div className="container-nav">
+                    <ul>
+                        <li>
+                            <NavLink 
+                                exact="true" 
+                                className={({ isActive }) =>
+                                    isActive ? "active-link" : "inactive-link"
+                                }
+                                to={ "/" }
+                            >
+                            Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink 
+                                exact="true" 
+                                className={({ isActive }) =>
+                                    isActive ? "active-link" : "inactive-link"
+                                }
+                                to={ "/news" }
+                            >
+                            News
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink 
+                                exact="true" 
+                                className={({ isActive }) =>
+                                    isActive ? "active-link" : "inactive-link"
+                                }
+                                to={ "/about" }
+                            >
+                            About
+                            </NavLink>
+                        </li>
+                    </ul>
+                    <img className="navbar-img" src={ img } />
+                    <ul>
                         { !this.props.name && <li><NavLink exact="true" to={ "/login" }>Login</NavLink></li> }
                         { !this.props.name && <li><NavLink exact="true" to={ "/signup" }>Sign Up</NavLink></li> }
                         { this.props.name && <li><NavLink exact="true" to={ "/user" }>Hello, { this.props.name }</NavLink></li> }
