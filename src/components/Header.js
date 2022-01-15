a9bd7114b639728bb9c3ccc13ac1e9b2b4e7c177
jpduong12 <jpduong12@gmail.com>
2022-01-15 01:03:29 -0800

    Media queries and animation done for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,52 +20,49 @@ export default class Header extends React.Component {
                     <div className="container-header-logo left">
                         <img className="header-logo" src={ img }/>
                     </div>
-                    <div className="container-dropdown right">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                    <nav className="container-list">
-                        <ul className="list-links dropdown">
-                            <li>
-                                <NavLink 
-                                    exact="true" 
-                                    className={({ isActive }) =>
-                                        isActive ? "active link" : "inactive link"
-                                    }
-                                    to={ "/" }
-                                >
-                                Home
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink 
-                                    exact="true" 
-                                    className={({ isActive }) =>
-                                        isActive ? "active link" : "inactive link"
-                                    }
-                                    to={ "/news" }
-                                >
-                                News
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink 
-                                    exact="true" 
-                                    className={({ isActive }) =>
-                                        isActive ? "active link" : "inactive link"
-                                    }
-                                    to={ "/about" }
-                                >
-                                About
-                                </NavLink>
-                            </li>
-                            { !this.props.name && <li><NavLink className="link" exact="true" to={ "/login" }>Login</NavLink></li> }
-                            { !this.props.name && <li><NavLink className="link"exact="true" to={ "/signup" }>Sign Up</NavLink></li> }
-                            { this.props.name && <li><NavLink className="link" exact="true" to={ "/user" }>Hello, { this.props.name }</NavLink></li> }
-                            { this.props.name && <li><button className="link" onClick={ this.handleLogoutClick.bind(this) }>Logout</button></li> }
-                        </ul>
-                    </nav>
+                    <input className="toggle-dropdown" type="checkbox" />
+                    <span className="span1"></span>
+                    <span className="span2"></span>
+                    <span className="span3"></span>
+                    <ul className="list-links dropdown">
+                        <li>
+                            <NavLink 
+                                exact="true" 
+                                className={({ isActive }) =>
+                                    isActive ? "active link" : "inactive link"
+                                }
+                                to={ "/" }
+                            >
+                            Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink 
+                                exact="true" 
+                                className={({ isActive }) =>
+                                    isActive ? "active link" : "inactive link"
+                                }
+                                to={ "/news" }
+                            >
+                            News
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink 
+                                exact="true" 
+                                className={({ isActive }) =>
+                                    isActive ? "active link" : "inactive link"
+                                }
+                                to={ "/about" }
+                            >
+                            About
+                            </NavLink>
+                        </li>
+                        { !this.props.name && <li><NavLink className="link" exact="true" to={ "/login" }>Login</NavLink></li> }
+                        { !this.props.name && <li><NavLink className="link"exact="true" to={ "/signup" }>Register</NavLink></li> }
+                        { this.props.name && <li><NavLink className="link" exact="true" to={ "/user" }>Hello, { this.props.name }</NavLink></li> }
+                        { this.props.name && <li><button className="link" onClick={ this.handleLogoutClick.bind(this) }>Logout</button></li> }
+                    </ul>
                 </div>
             </header>
         );
