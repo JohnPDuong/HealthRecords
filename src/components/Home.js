8714710477baf0f8bda390f567bb2bf8ce560fa3
jpduong12 <jpduong12@gmail.com>
2022-01-17 15:49:06 -0800

    Improved home page css

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,18 +17,36 @@ export default class Home extends React.Component {
                     </div>
                 </div>
                 <h3 className="aim-header">What is our AIM?</h3>
-                <div className="row">
-                    <div className="column">
-                        <h4>Accessibilty</h4>
-                        <p>Some words about accessibility...</p>
+                <div className="row main-aim">
+                    <div className="column divider">
+                        <div className="main-aim-text">
+                            <h4 className="main-aim-text-header">Accessibilty</h4>
+                            <ul>
+                                <li>Providing accurate, up-to-date, and complete information about patients at the point of care</li>
+                                <li>Enabling quick access to patient records for more coordinated, efficient care</li>
+                                <li>Enabling safer, more reliable prescribing</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="column divider">
+                        <div className="main-aim-text">
+                            <h4 className="main-aim-text-header">Integration</h4>
+                            <ul>
+                                <li>Helping promote legible, complete documentation to other clinics and clinicians</li>
+                                <li>Improving patient and provider interaction and communication, as well as health care convenience</li>
+                                <li>Helping providers improve productivity and work-life balance</li>
+                            </ul>
+                        </div>
                     </div>
                     <div className="column">
-                        <h4>Integration</h4>
-                        <p>Some words about integration...</p>
-                    </div>
-                    <div className="column">
-                        <h4>Management</h4>
-                        <p>Some words about management...</p>
+                        <div className="main-aim-text">
+                            <h4 className="main-aim-text-header">Management</h4>
+                            <ul>
+                                <li>Enhancing privacy and security of patient data</li>
+                                <li>Helping promote legible, complete documentation and accurate, streamlined coding and billing</li>
+                                <li>Reducing costs through decreased paperwork, improved safety, reduced duplication of testing, and improved health.</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </Root>
